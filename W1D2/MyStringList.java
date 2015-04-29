b4064645c7fd2db260ed346531b6bd65a6b75c8b
Ctwice <carpe359@gmail.com>
2015-04-29 09:51:07 -0500

    Tuesday
    Practice

---
+++ W1D2/MyStringList.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuesday;
+
+/**
+ *
+ * @author Pandoras_Box
+ */
+public class MyStringList {
+
+    private final String[] strArray = new String[50];
+    private int size;
+
+    public MyStringList() {
+    }
+
+    public void add(String s) {
+    }
+
+    public void get(int i) {
+    }
+
+    public boolean find(String s) {
+        return false;
+        
+    }
+
+    public void insert(String s, int pos) {
+    }
+
+    public boolean remove(String s) {
+        return false;
+    }
+
+    private void resize() {
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void display() {
+    }
+
+    public static void main(String args[]) {
+        MyStringList obj = new MyStringList();
+    }
+
+}
---
+++ W1D2/RandomArray.java
@@ -0,0 +1,27 @@
+
+import static java.lang.Math.random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Pandoras_Box
+ */
+public class RandomArray {
+
+    public static void main(String[] args) {
+
+        RandomArray[] nums = new RandomArray[50];
+
+        for(i=0;i<nums.length;i++){
+            nums[i] = Math.random()*100;
+        }
+
+ 
+            
+        }
+    }
